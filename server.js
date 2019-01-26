77e5413171c5d4e18782ef2ce585cfdc174d9c8f
Lucy <lucyhyw@gmail.com>
2019-01-26 14:21:32 -0500

    initial files

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const assert = require('assert');
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:aEpNJ0tCWxo00jvm@cluster0-qapyv.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+ // perform actions on the collection object
+  client.close();
+});
+
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
+
